17308ed4d3f91278279e252a70d5d52e3288d85d
RoanKelder <rkelder@roc-dev.com>
2020-04-15 12:01:42 +0200

    H05 Opdracht 1

---
+++ H05/responseOpdr2.php
@@ -0,0 +1,25 @@
+<?php
+
+if($_POST['voornaam'] == "") {
+    echo "Voornaam is niet ingevuld";
+    echo "<br><a href=\"formulierOpdr2.html\"> Terug naar het formulier</a>";
+}
+
+if ($_POST['wachtwoord'] == ""){
+    echo "wachtwoord is niet ingevuld";
+    echo "<br><a href=\"formulierOpdr2.html\"> Terug naar het formulier</a>";
+}
+
+if ($_POST['adres'] == ""){
+    echo "adres is niet ingevuld";
+    echo "<br><a href=\"formulierOpdr2.html\"> Terug naar het formulier</a>";
+}
+
+if ($_POST['mail'] == ""){
+    echo "mail is niet ingevuld";
+    echo "<br><a href=\"formulierOpdr2.html\"> Terug naar het formulier</a>";
+}
+
+
+
+
